5709f6fc83f88f346e06fa7ffe913e83bfdf786f
zongshun <zhangzs@bu.edu>
2023-05-09 22:36:23 +0000

    learner script

--- run_learner.sh
+++ run_learner.sh
@@ -1,14 +1,4 @@
-# allThreads=(1 2 3 4)
-# for i in ${allThreads[@]};
-# do
-#     echo $i
-#     /home/cc/FSL/env/bin/python3 /home/cc/split-learning/main_m2_20_equal_dataset.py pc cifar10 $i &
-#     # /home/cc/FSL/env/bin/python3 /home/cc/split-learning/main_m2_20_equal_dataset.py pc cifar10 $i
-# done
-
-/home/cc/FSL/env/bin/python3 learner_script.py cifar10 0 CUTS 4 &
-/home/cc/FSL/env/bin/python3 learner_script.py cifar10 1 CUTS 7 &
-/home/cc/FSL/env/bin/python3 learner_script.py cifar10 2 CUTS 16 &
-/home/cc/FSL/env/bin/python3 learner_script.py cifar10 3 CUTS 30 &
+python3 learner_script.py mnist 0 CUTS 6 &
+python3 learner_script.py mnist 1 CUTS 6 &
 
 wait
